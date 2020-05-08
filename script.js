e59b156bae0cff7be89a2d473fa3e7cdc29c0cab
RomJosh99 <63078615+RomJosh99@users.noreply.github.com>
2020-05-08 10:17:55 -0500

    Changes
    Changes

--- script.js
+++ script.js
@@ -1,25 +1,31 @@
-var settings = {
-	"async": true,
-	"crossDomain": true,
-	"url": "https://love-calculator.p.rapidapi.com/getPercentage?fname=Mike&sname=Amiri",
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "love-calculator.p.rapidapi.com",
-		"x-rapidapi-key": "22c1cff26fmshe1d921a4c29167bp177f37jsn2d77eedbed64"
-	}
-}
 
-$.ajax(settings).done(function (response) {
-	console.log(response);
-});
-
-
-
- 
 //Form Script
-var yourName = "Mike";  
-var cushName = 'Amiri';  
+var yourName = "";  
+var crushName = "";  
 
-document.getElementById("13%").innerHTML =
+document.getElementById("percentage").innerHTML =
 yourName + " " + crushName; 
-//Submit Button --- Find love!<3
+$("#Btn").on("click" ,function(){
+	event.preventDefault();
+	var yourName = $("#name").val();  
+    var crushName = $("#crush").val();  
+	var settings = {
+		"async": true,
+		"crossDomain": true,
+		"url": `https://love-calculator.p.rapidapi.com/getPercentage?fname=${yourName}&sname=${crushName}`,
+		"method": "GET",
+		"headers": {
+			"x-rapidapi-host": "love-calculator.p.rapidapi.com",
+			"x-rapidapi-key": "22c1cff26fmshe1d921a4c29167bp177f37jsn2d77eedbed64"
+		}
+	}
+	
+	$.ajax(settings).done(function (response) {
+		console.log(response);
+		document.getElementById("percentage").innerHTML =
+        $("#yourName")+" "+$("#crushName");
+	});
+	
+
+
+}) 
